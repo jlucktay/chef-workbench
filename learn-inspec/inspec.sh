7a8bdb112415d6a48a135eea3c5ec2a56437e37a
James Lucktaylor <jlucktay+github@gmail.com>
2018-11-23 11:31:36 +0000

    Added command history from second tutorial

--- learn-inspec/inspec.sh
+++ learn-inspec/inspec.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+# First tutorial
+wget -O docker-compose.yml https://raw.githubusercontent.com/learn-chef/inspec/master/docker-compose.yml
 docker-compose pull
 docker-compose up -d
 docker exec -it workstation bash
 docker-compose down
+
+# Second tutorial
+wget -O docker-compose-nginx.yml https://raw.githubusercontent.com/learn-chef/inspec/master/docker-compose-nginx.yml
+docker-compose -f docker-compose-nginx.yml pull
+docker-compose -f docker-compose-nginx.yml up -d
+docker exec -it workstation bash
+docker-compose -f docker-compose-nginx.yml down
